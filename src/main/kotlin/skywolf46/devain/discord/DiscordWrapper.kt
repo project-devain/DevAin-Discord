0303bb04ef58fb0c310483876fa3d90fbcebf2b8
skywolf46 <skywolf461@gmail.com>
2024-07-18 18:14:43 +0900

    feat: DiscordWrapper에 DiscordFallback 래핑 메서드 추가

--- src/main/kotlin/skywolf46/devain/discord/DiscordWrapper.kt
+++ src/main/kotlin/skywolf46/devain/discord/DiscordWrapper.kt
@@ -10,23 +10,18 @@ import skywolf46.devain.discord.command.BasicDiscordCommand
 import skywolf46.devain.discord.command.EnhancedDiscordCommand
 import skywolf46.devain.discord.data.lifecycle.DiscordFallback
 
-class DiscordWrapper(val jda: JDA, fallback: DiscordFallback) {
+class DiscordWrapper(val jda: JDA, val fallback: DiscordFallback = DiscordFallback()) {
     companion object {
         @JvmStatic
         fun withToken(
             token: String,
-            fallback: DiscordFallback,
             vararg intent: GatewayIntent = GatewayIntent.values(),
-            onInitialize: (JDABuilder) -> Unit = {}
+            onInitialize: (JDABuilder) -> Unit = {},
+            fallback: DiscordFallback = DiscordFallback()
         ): DiscordWrapper {
             return DiscordWrapper(
                 JDABuilder.create(token, intent.toList()).apply(onInitialize).build().awaitReady(), fallback
             )
-        }
-
-        @JvmStatic
-        fun builder(): Builder {
-            return Builder()
         }
     }
 
@@ -47,59 +42,25 @@ class DiscordWrapper(val jda: JDA, fallback: DiscordFallback) {
         return this
     }
 
+
+
+    fun fallbackButton(key: String, fallback: (ButtonInteractionEvent) -> Unit): DiscordWrapper {
+        this.fallback.buttonFallback(key, fallback)
+        return this
+    }
+
+    fun fallbackStringSelection(key: String, fallback: (StringSelectInteractionEvent) -> Unit): DiscordWrapper {
+        this.fallback.stringSelectionFallback(key, fallback)
+        return this
+    }
+
+    fun fallbackSelection(key: String, fallback: (EntitySelectInteractionEvent) -> Unit): DiscordWrapper {
+        this.fallback.selectionFallback(key, fallback)
+        return this
+    }
+
     fun finishSetup() {
         jda.addEventListener(commandAdapter)
         commandAdapter.registerCommands(*commands.toTypedArray())
-    }
-
-    class Builder {
-        private val commands = mutableListOf<BasicDiscordCommand>()
-        private var fallback: DiscordFallback.Builder = DiscordFallback.builder()
-        private var token: String? = null
-        private var onInitialize: (JDABuilder) -> Unit = {}
-
-        fun fallback(fallback: DiscordFallback.Builder): Builder {
-            this.fallback = fallback
-            return this
-        }
-
-        fun fallbackButton(key: String, fallback: (ButtonInteractionEvent) -> Unit): Builder {
-            this.fallback.buttonFallback(key, fallback)
-            return this
-        }
-
-        fun fallbackStringSelection(key: String, fallback: (StringSelectInteractionEvent) -> Unit): Builder {
-            this.fallback.stringSelectionFallback(key, fallback)
-            return this
-        }
-
-        fun fallbackSelection(key: String, fallback: (EntitySelectInteractionEvent) -> Unit): Builder {
-            this.fallback.selectionFallback(key, fallback)
-            return this
-        }
-
-
-        fun token(token: String): Builder {
-            this.token = token
-            return this
-        }
-
-        fun onInitialize(onInitialize: (JDABuilder) -> Unit): Builder {
-            this.onInitialize = onInitialize
-            return this
-        }
-
-        fun registerCommands(vararg command: BasicDiscordCommand): Builder {
-            commands.addAll(command)
-            return this
-        }
-
-        fun build(): DiscordWrapper {
-            return withToken(
-                token ?: throw IllegalStateException("Token is not provided."), fallback.build()
-            ) {
-                onInitialize.invoke(it)
-            }.registerCommands(*commands.toTypedArray())
-        }
     }
 }
